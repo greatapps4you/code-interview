880de57c7a372cde1831f81ff7754c65bcc9f74e
Jose E. de Souza N <developer@greatapps4you.us>
2020-09-19 00:42:43 +0200

    [CODE-INTERVIEW] First Question: Number Imcrementer

---
+++ src/main/java/us/greatapps4you/codeinterview/corejava/NumberIncrementer.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020 GreatApps4you LLC All rights reserved
+ * https://greatapps4you.us
+ * CSSML NDSMD VRS + NSMV SMQL IVB
+ */
+
+package us.greatapps4you.codeinterview.corejava;
+
+/**
+ * Given an Array of Integers representing a number,
+ * increment that array in 1 unit at each method call
+ * so that the whole number increases one unit.
+ */
+public class NumberIncrementer {
+
+}
---
+++ src/test/java/us/greatapps4you/codeinterview/corejava/NumberIncrementerTest.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020 GreatApps4you LLC All rights reserved
+ * https://greatapps4you.us
+ * CSSML NDSMD VRS + NSMV SMQL IVB
+ */
+
+package us.greatapps4you.codeinterview.corejava;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberIncrementerTest {
+
+}
