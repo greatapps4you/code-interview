213bc8603ede356d0e5243812d2d50327d45fe45
Jose E. de Souza N <developer@greatapps4you.us>
2020-09-19 16:59:31 +0200

    [CODE-INTERVIEW] First Question: Number Imcrementer: with primitives. only one test passes so far.

--- src/main/java/us/greatapps4you/codeinterview/corejava/NumberIncrementerWithPrimitives.java
+++ src/main/java/us/greatapps4you/codeinterview/corejava/NumberIncrementerWithPrimitives.java
@@ -13,30 +13,36 @@ public class NumberIncrementerWithPrimitives {
 
         int[] output = Arrays.copyOf(input, input.length);
 
-        int LAST_DIGIT_INDEX = output.length - 1;
+        final int LAST_DIGIT_INDEX = output.length - 1;
         int CURRENT_DIGIT_INDEX = input.length - 1;
         int carry = 0;
 
         while (CURRENT_DIGIT_INDEX >= 0) {
-            System.out.println(input[CURRENT_DIGIT_INDEX]);
-            int incrementedValue = input[CURRENT_DIGIT_INDEX] + 1;
 
-            if (carry > 0) {
-                output[CURRENT_DIGIT_INDEX] = input[CURRENT_DIGIT_INDEX] + carry;
-            }
-
-            carry = (incrementedValue - 10) + 1;
-
-            if (carry > 0) {
-                output[CURRENT_DIGIT_INDEX] = 0;
-            } else if (CURRENT_DIGIT_INDEX == LAST_DIGIT_INDEX) {
-                output[CURRENT_DIGIT_INDEX] = incrementedValue;
-                carry = 0;
+            //First of all let's treat the unity digit
+            if (CURRENT_DIGIT_INDEX == LAST_DIGIT_INDEX) {
+                if ((input[CURRENT_DIGIT_INDEX] + carry - 10) + 1 > 0) {
+                    output[CURRENT_DIGIT_INDEX] = 0;
+                } else {
+                    output[CURRENT_DIGIT_INDEX] = input[CURRENT_DIGIT_INDEX] + 1;
+                }
             } else {
-                output[CURRENT_DIGIT_INDEX] = input[CURRENT_DIGIT_INDEX];
+                if (carry > 0) {
+                    //Let's take carry in
+                    int valueWithCarry = input[CURRENT_DIGIT_INDEX] + carry;
+
+                    if ((valueWithCarry - 10) + 1 > 0) {
+                        output[CURRENT_DIGIT_INDEX] = 0;
+                    } else {
+                        output[CURRENT_DIGIT_INDEX] = valueWithCarry;
+                    }
+                } else {
+                    output[CURRENT_DIGIT_INDEX] = input[CURRENT_DIGIT_INDEX];
+                }
             }
-
-
+            //Always Calculate carry
+            carry = (input[CURRENT_DIGIT_INDEX] + carry - 10) + 1;
+            //Go towards the first INDEX
             CURRENT_DIGIT_INDEX--;
         }
 
--- src/test/java/us/greatapps4you/codeinterview/corejava/NumberIncrementerWithPrimitivesTest.java
+++ src/test/java/us/greatapps4you/codeinterview/corejava/NumberIncrementerWithPrimitivesTest.java
@@ -6,6 +6,7 @@
 
 package us.greatapps4you.codeinterview.corejava;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,18 +14,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NumberIncrementerWithPrimitivesTest {
 
-    private NumberIncrementerWithPrimitives numberIncrementerWithPrimitives;
+    private NumberIncrementerWithPrimitives numberIncrementer;
 
     @BeforeEach
     void setUp() {
-        this.numberIncrementerWithPrimitives = new NumberIncrementerWithPrimitives();
+        this.numberIncrementer = new NumberIncrementerWithPrimitives();
     }
 
     @Test
     void givenNumberArrayWhenIncrementedThenReturnIncrementedArray() {
         int[] given = {1, 2, 3};
-        int[] actual = numberIncrementerWithPrimitives.increment(given);
+        int[] actual = numberIncrementer.increment(given);
         int[] expected = {1, 2, 4};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void givenLargeArrayOfNineWhenIncrementedThenReturnIncrementedArray() {
+        int[] given = {9, 9, 9, 9, 9, 9, 9};
+        int[] actual = numberIncrementer.increment(given);
+        int[] expected = {1, 0, 0, 0, 0, 0, 0, 0};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void giveMixedArrayOfNumbersWhenIncrementedThenReturnIncrementedArray() {
+        int[] given = {1,3,8,9};
+        int[] actual = numberIncrementer.increment(given);
+        int[] expected = {1,3,9,0};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void giveTooLargeArrayOfNumbersWhenIncrementedThenReturnIncrementedArray() {
+        int[] given = {1,3,8,9,5,4,3,7,8,6,5,3,2,4,5,6,7,8,0,9};
+        int[] actual = numberIncrementer.increment(given);
+        int[] expected = {1,3,8,9,5,4,3,7,8,6,5,3,2,4,5,6,7,8,1,0};
         assertArrayEquals(expected, actual);
     }
 
