759693d287196de3cc8260eedc220db213989743
Jose E. de Souza N <developer@greatapps4you.us>
2020-09-19 08:15:00 +0200

    [CODE-INTERVIEW] First Question: Number Imcrementer: edge case test pass.

--- src/test/java/us/greatapps4you/codeinterview/corejava/NumberIncrementerTest.java
+++ src/test/java/us/greatapps4you/codeinterview/corejava/NumberIncrementerTest.java
@@ -6,10 +6,11 @@
 
 package us.greatapps4you.codeinterview.corejava;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class NumberIncrementerTest {
 
@@ -42,6 +43,12 @@ class NumberIncrementerTest {
         int[] actual = numberIncrementer.increment(given);
         int[] expected = {1,3,9,0};
         assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void giveTooLargeArrayOfNumbersWhenIncrementedThenThrowNumberFormatException() {
+        int[] given = {1,3,8,9,5,4,3,7,8,6,5,3,2,4,5,6,7,8,0,9};
+        Assertions.assertThrows(NumberFormatException.class, () -> numberIncrementer.increment(given));
     }
 
 }
