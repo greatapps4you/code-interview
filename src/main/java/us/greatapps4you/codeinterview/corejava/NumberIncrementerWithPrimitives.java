ec82bb7a32ca4823b75294befb762c1e8f767e2e
Jose Esteves de Souza Neto <jose.desouzaneto.ext@dkv-mobility.com>
2020-09-21 16:41:11 +0200

    [CORE-JAVA] Revisiting NumberIncrementerWithPrimitives

--- src/main/java/us/greatapps4you/codeinterview/corejava/NumberIncrementerWithPrimitives.java
+++ src/main/java/us/greatapps4you/codeinterview/corejava/NumberIncrementerWithPrimitives.java
@@ -9,41 +9,69 @@ package us.greatapps4you.codeinterview.corejava;
 import java.util.Arrays;
 
 public class NumberIncrementerWithPrimitives {
+
+    /**
+     * Algorithm: increment number in Array of int form in 1 unit and output array of int
+     *
+     * @param input
+     * @return
+     */
     public int[] increment(int[] input) {
 
+        // 0 - Initial state
         int[] output = Arrays.copyOf(input, input.length);
+        final int FIRST_DIGIT_INDEX = 0;
+        final int LAST_DIGIT_INDEX = input.length - 1;
+        int currentDigitIndex = input.length - 1;
+        int carryOne = 0;
 
-        final int LAST_DIGIT_INDEX = output.length - 1;
-        int CURRENT_DIGIT_INDEX = input.length - 1;
-        int carry = 0;
+        // 1 - Iterate over the input from right to left
+        while (currentDigitIndex >= 0) {
 
-        while (CURRENT_DIGIT_INDEX >= 0) {
+            // 2 - Increment the current digit
+            int currentDigitIncremented = input[currentDigitIndex] + 1;
 
-            //First of all let's treat the unity digit
-            if (CURRENT_DIGIT_INDEX == LAST_DIGIT_INDEX) {
-                if ((input[CURRENT_DIGIT_INDEX] + carry - 10) + 1 > 0) {
-                    output[CURRENT_DIGIT_INDEX] = 0;
+            // 3 - Treat carry
+            carryOne = currentDigitIncremented >= 10 ? 1 : 0;
+
+                if (carryOne == 1) {
+                    output[currentDigitIndex] = (currentDigitIncremented - 10) + carryOne;
                 } else {
-                    output[CURRENT_DIGIT_INDEX] = input[CURRENT_DIGIT_INDEX] + 1;
+                    output[currentDigitIndex] = input[currentDigitIndex];
                 }
-            } else {
-                if (carry > 0) {
-                    //Let's take carry in
-                    int valueWithCarry = input[CURRENT_DIGIT_INDEX] + carry;
+//            //First of all let's treat the unity digit
+//            if (currentDigitIndex == LAST_DIGIT_INDEX) {
+//                if ((input[currentDigitIndex] + carry - 10) + 1 > 0) {
+//                    output[currentDigitIndex] = 0;
+//                } else {
+//                    output[currentDigitIndex] = input[currentDigitIndex] + 1;
+//                }
+//            } else {
+//                if (carry > 0) {
+//                    //Let's take carry in
+//                    int valueWithCarry = input[currentDigitIndex] + carry;
+//
+//                    if ((valueWithCarry - 10) + 1 > 0) {
+//                        output[currentDigitIndex] = 0;
+//                    } else {
+//                        output[currentDigitIndex] = valueWithCarry;
+//                    }
+//                } else {
+//                    output[currentDigitIndex] = input[currentDigitIndex];
+//                }
+//            }
+//            //Always Calculate carry
+//            carry = (input[currentDigitIndex] + carry - 10) + 1;
+//            //Go towards the first INDEX
 
-                    if ((valueWithCarry - 10) + 1 > 0) {
-                        output[CURRENT_DIGIT_INDEX] = 0;
-                    } else {
-                        output[CURRENT_DIGIT_INDEX] = valueWithCarry;
-                    }
-                } else {
-                    output[CURRENT_DIGIT_INDEX] = input[CURRENT_DIGIT_INDEX];
-                }
-            }
-            //Always Calculate carry
-            carry = (input[CURRENT_DIGIT_INDEX] + carry - 10) + 1;
-            //Go towards the first INDEX
-            CURRENT_DIGIT_INDEX--;
+
+            System.out.println(output[currentDigitIndex]);
+            System.out.println("Carry: " + carryOne);
+
+            // 3 - Must carry on?
+
+
+            currentDigitIndex--;
         }
 
         return output;
